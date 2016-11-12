cc7124eee3668506df9d3089af74926ca1036d7e
spin13 <spin.eco.info@gmail.com>
2016-11-12 15:44:55 +0900

    first

---
+++ env.py
@@ -0,0 +1,1 @@
+﻿SLACK_TOKEN = ''
---
+++ slack.py
@@ -0,0 +1,17 @@
+﻿
+import env
+import requests
+
+
+SLACK_TOKEN = env.SLACK_TOKEN
+
+def post_slack(username='', channel='', message=''):
+    url = 'https://slack.com/api/chat.postMessage'
+    params ={
+        'token': SLACK_TOKEN,
+        'channel': channel,
+        'text': message,
+        'username': username
+    }
+    res = requests.post(url, params=params)
+    return res
